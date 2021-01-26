04006972533cb36afd13a1b607e32debf60d461d
Dung Tran <pdung1989@gmail.com>
2021-01-26 14:54:31 +0200

    create BuildControl component

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './buildControl.css';
+
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div classes={classes.Label}>{props.label}</div>
+    <button classes={classes.Less}>Less</button>
+    <button classes={classes.More}>More</button>
+  </div>
+);
+
+
+export default buildControl;
