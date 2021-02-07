8de66fd079a56ce61d0a6f1af63a3a0b25cc4cfc
Dung Tran <pdung1989@gmail.com>
2021-02-07 22:09:08 +0200

    make Modal as class based component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
@@ -10,20 +10,21 @@ const orderSummary = (props) => {
         <li>
           <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
         </li>  );    
-    } );
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-}
+    });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+        <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+};
 
 export default orderSummary;
